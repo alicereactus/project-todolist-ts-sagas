a9f27a855b0328e51b197b12aa58c615234bcf98
alicereactus <agamia@mail.ru>
2021-05-14 22:56:00 +0300

    Updates

---
+++ precache-manifest.f8f4af17c130f748c52d7bf7b9b4cb6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34cf34f16c6dbcf1162a4e4bcfdd7bd",
+    "url": "/project-todolist-ts-sagas/index.html"
+  },
+  {
+    "revision": "99c454bad970c8c0ce18",
+    "url": "/project-todolist-ts-sagas/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "792095107032a412aca7",
+    "url": "/project-todolist-ts-sagas/static/js/2.fb296b1c.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/project-todolist-ts-sagas/static/js/2.fb296b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c454bad970c8c0ce18",
+    "url": "/project-todolist-ts-sagas/static/js/main.af0c4ac1.chunk.js"
+  },
+  {
+    "revision": "15a763bc03292b6cb99d",
+    "url": "/project-todolist-ts-sagas/static/js/runtime-main.69c8464a.js"
+  }
+]);
